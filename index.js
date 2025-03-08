329d513e81c70e1b3968a62fe025406022973401
Tuanlong22 <longcrisro7@gmail.com>
2025-03-08 12:52:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const envelope = document.querySelector('.envelope-wrapper');
+        envelope.addEventListener('click', () => {
+            envelope.classList.toggle('flap');
+        });
+
+function openCanvas() {
+            document.getElementById("letterCanvas").classList.add("active");
+        }
+
+function closeCanvas() {
+            document.getElementById("letterCanvas").classList.remove("active");
+        }
+
+function Thanks(){
+    document.getElementById("letterCanvas").classList.add("active");
+}
+
+function openCanvas2() {
+    document.getElementById("canvas2").classList.add("active");
+}
+
+function closeAllCanvas() {
+    document.getElementById("canvas1").classList.remove("active");
+    document.getElementById("canvas2").classList.remove("active");
+}
